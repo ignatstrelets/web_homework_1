5c0aeca87b70c5967491dffa08c150bc93841840
ignatstrelets <120760559+ignatstrelets@users.noreply.github.com>
2023-11-08 14:13:21 +0300

    Update function_1_3.js

--- awesomeProject/js/function_1_3.js
+++ awesomeProject/js/function_1_3.js
@@ -17,7 +17,7 @@ const node_for_click_all = document.getElementById("for_click_all");
   function find_edit_all() {
     document.getElementsByTagName('p')[0].innerText = 'Strelets'
     document.getElementsByTagName('p')[1].innerText = 'Ignat'
-    document.getElementsByTagName('p')[2].innerText = ''
+    document.getElementsByTagName('p')[2].innerText = ' '
     document.getElementsByTagName('p')[3].innerText = '08/26/2003'
   }
 node_for_click_all.addEventListener("click",find_edit_all)
